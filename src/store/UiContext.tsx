e90dde080d8829206a644541f46726f17d437982
Gabriel <gabriel1997.castro@gmail.com>
2022-04-02 17:08:03 -0300

    Create global state manager for ui

---
+++ src/store/UiContext.tsx
@@ -0,0 +1,42 @@
+import { UiState } from '../types/ui';
+import {
+  createContext,
+  FunctionComponent,
+  memo,
+  SetStateAction,
+  useContext,
+  useState,
+  Dispatch,
+} from 'react';
+
+type UiStateUpdater = [
+  UiState,
+  Dispatch<SetStateAction<UiState>>
+];
+
+type SetNewUiState = (arg0: UiState) => void;
+type UiContextHook = () => [UiState, SetNewUiState];
+const initialState: UiState = {};
+
+export const UiContext = createContext<UiStateUpdater | UiState>(
+  initialState
+);
+
+export const UiContextProvider: FunctionComponent = memo(
+  ({ children }) => {
+    const StateGetSet = useState(initialState);
+    return (
+      <UiContext.Provider value={StateGetSet}>
+        {children}
+      </UiContext.Provider>
+    );
+  }
+);
+
+export const useUiContext: UiContextHook = () => {
+  const [state, setState] = useContext(UiContext) as UiStateUpdater;
+  const setNewState = (newState: UiState) => {
+    setState((prevState) => ({ ...prevState, ...newState }));
+  };
+  return [state, setNewState];
+};
